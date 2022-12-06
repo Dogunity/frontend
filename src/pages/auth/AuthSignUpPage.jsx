2729fb0f27e0fed515cce9fa44692854309c8b38
foxmon <jjockrod@naver.com>
2022-12-06 22:21:09 +0900

    Fix: AuthSignUpPage

--- src/pages/auth/AuthSignUpPage.jsx
+++ src/pages/auth/AuthSignUpPage.jsx
@@ -1,7 +1,17 @@
+import useSignUp from "../../hooks/auth/useSignUp";
 import AuthContainer from "../../components/containers/AuthContainer";
 
 const AuthSignUpPage = () => {
-  return <AuthContainer isSignIn={false} register={register} errors={errors} />;
+  const { register, errors, handleSubmit, handleRegisterSubmit } = useSignUp();
+
+  return (
+    <AuthContainer
+      isSignIn={false}
+      register={register}
+      errors={errors}
+      onRegisterRequestEvent={handleSubmit(handleRegisterSubmit)}
+    />
+  );
 };
 
 export default AuthSignUpPage;
