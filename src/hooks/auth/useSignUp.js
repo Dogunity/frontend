576eef982181f71898c604c6d1dbc04f8fe5b2f5
foxmon <jjockrod@naver.com>
2022-12-06 22:12:11 +0900

    Feat: useSignUp

---
+++ src/hooks/auth/useSignUp.js
@@ -0,0 +1,42 @@
+import { useCallback } from "react";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers";
+import { useForm } from "react-hook-form";
+
+const userSignUpSchema = yup.object().shape({
+  email: yup.string().email("Please check your email information"),
+  nickname: yup
+    .string()
+    .min(4, "Nickname must be 4 characters or more")
+    .max(15, "Nickname must be 15 characters or less"),
+  password: yup
+    .string()
+    .min(8, "Please check your password information")
+    .max(15, "Please check your password information")
+    .matches(/(?=.*\d)(?=.*[a-z]).{8,15}/),
+});
+
+const useSignUp = () => {
+  const { register } = useForm({
+    resolver: yupResolver(userSignUpSchema),
+    mode: "onChange",
+    defaultValues: {
+      email: "",
+      nickname: "",
+      password: "",
+    },
+  });
+
+  const handleRegisterSubmit = useCallback((data) => {
+    console.log(data);
+  }, []);
+
+  return {
+    register,
+    errors,
+    handleSubmit,
+    handleRegisterSubmit,
+  };
+};
+
+export default useSignUp;
