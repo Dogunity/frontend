df823f40e49ae0a0f207fe5168073528d909c370
foxmon <jjockrod@naver.com>
2022-12-08 00:03:08 +0900

    Fix: CommunityCardImage

--- src/components/community/CommunityCardImage.jsx
+++ src/components/community/CommunityCardImage.jsx
@@ -4,9 +4,6 @@ const CommunityCardImage = () => {
   return (
     <CardMedia
       component="img"
-      sx={{
-        pt: "56.25%",
-      }}
       image="https://source.unsplash.com/random"
       alt="random"
     />
