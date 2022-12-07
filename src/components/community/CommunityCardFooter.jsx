a67cdf4e9e811d5382fb297056268737eae06865
foxmon <jjockrod@naver.com>
2022-12-07 21:48:51 +0900

    Feat: CommunityCardFooter

---
+++ src/components/community/CommunityCardFooter.jsx
@@ -0,0 +1,12 @@
+import { CardActions, Button } from "@mui/material";
+
+const CommunityCardFooter = () => {
+  return (
+    <CardActions>
+      <Button size="small">View</Button>
+      <Button size="small">Edit</Button>
+    </CardActions>
+  );
+};
+
+export default CommunityCardFooter;
