0d0b4865c39ac5a7a45a28b294cc4969c1c97a09
foxmon <jjockrod@naver.com>
2022-12-07 22:18:04 +0900

    Fix: MyRouter

--- src/MyRouter.jsx
+++ src/MyRouter.jsx
@@ -5,6 +5,8 @@ import Loader from "./components/loader/Loader";
 const MainPage = lazy(() => import("./pages/main/MainPage"));
 const AuthSignInPage = lazy(() => import("./pages/auth/AuthSignInPage"));
 const AuthSignUpPage = lazy(() => import("./pages/auth/AuthSignUpPage"));
+const CommunityPage = lazy(() => import("./pages/community/CommunityPage"));
+const FeedPage = lazy(() => import("./pages/feed/FeedPage"));
 
 function MyRouter() {
   return (
@@ -14,6 +16,8 @@ function MyRouter() {
           <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<AuthSignInPage />} />
           <Route path="/register" element={<AuthSignUpPage />} />
+          <Route path="/community" element={<CommunityPage />} />
+          <Route path="/feed" element={<FeedPage />} />
         </Routes>
       </BrowserRouter>
     </Suspense>
