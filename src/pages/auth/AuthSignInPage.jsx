26ea4da39277c867b3e0232c44e051c4cca59e5c
foxmon <jjockrod@naver.com>
2022-12-07 23:35:22 +0900

    Fix: AuthSignInPage

--- src/pages/auth/AuthSignInPage.jsx
+++ src/pages/auth/AuthSignInPage.jsx
@@ -1,16 +1,37 @@
+import React from "react";
+import { Typography } from "@mui/material";
 import useSignIn from "../../hooks/auth/useSignIn";
 import AuthContainer from "../../components/containers/AuthContainer";
+import MyCustomModal from "../../components/modal/MyCustomModal";
 
 const AuthSignInPage = () => {
-  const { register, errors, handleSubmit, handleLoginSubmit } = useSignIn();
+  const {
+    register,
+    errors,
+    isModalOpen,
+    handleSubmit,
+    handleLoginSubmit,
+    handleIsModalOpenStateChange,
+  } = useSignIn();
 
   return (
-    <AuthContainer
-      isSignIn={true}
-      register={register}
-      errors={errors}
-      onUserLoginRequestEvent={handleSubmit(handleLoginSubmit)}
-    />
+    <React.Fragment>
+      <AuthContainer
+        isSignIn={true}
+        register={register}
+        errors={errors}
+        onUserLoginRequestEvent={handleSubmit(handleLoginSubmit)}
+      />
+      <MyCustomModal
+        isModalOpen={isModalOpen}
+        title="WARNING"
+        onIsModalOpenStateChangeEvent={handleIsModalOpenStateChange}
+      >
+        <Typography variant="h6" color="text.secondary">
+          Please check your information
+        </Typography>
+      </MyCustomModal>
+    </React.Fragment>
   );
 };
 
