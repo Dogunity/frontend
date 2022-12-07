c64bf1f744066e6b580db1a8731b8a36541de209
foxmon <jjockrod@naver.com>
2022-12-07 22:39:45 +0900

    Feat: usePagination

---
+++ src/hooks/pagination/usePagination.js
@@ -0,0 +1,39 @@
+import { useMemo, useCallback } from "react";
+
+const usePagination = (curPage, setCurPage, totalPage, divider) => {
+  const isFirstPage = useMemo(() => curPage === 1, [curPage]);
+  const isLastPage = useMemo(() => curPage === totalPage, [totalPage]);
+  const initialPageNum = (Math.ceil(curPage / divider) + 1) * divider + 1;
+  const pageListIndex = Array.from({ length: divider })
+    .fill(0)
+    .map((_, index) =>
+      initialPageNum + index <= totalPage ? initialPageNum + index : null
+    );
+  const pageList = pageListIndex.filter((page) => Number.isInteger(page));
+
+  const handlePrevPageButtonClick = useCallback(() => {
+    setCurPage(curPage - 1);
+  }, [curPage, setCurPage]);
+
+  const handlePageButtonClick = useCallback(
+    (page) => () => {
+      setCurPage(page);
+    },
+    [setCurPage]
+  );
+
+  const handleNextPageButtonClick = useCallback(() => {
+    setCurPage(curPage + 1);
+  }, [curPage, setCurPage]);
+
+  return {
+    isFirstPage,
+    isLastPage,
+    pageList,
+    handlePrevPageButtonClick,
+    handlePageButtonClick,
+    handleNextPageButtonClick,
+  };
+};
+
+export default usePagination;
