8d83e1266b2aa26e3b6374b477b9319e96b9953c
foxmon <jjockrod@naver.com>
2022-12-07 22:19:04 +0900

    Feat: FeedPage

---
+++ src/pages/feed/FeedPage.jsx
@@ -0,0 +1,12 @@
+import CommonContainer from "../../components/containers/CommonContainer";
+import FeedContainer from "../../components/containers/FeedContainer";
+
+const FeedPage = () => {
+  return (
+    <CommonContainer>
+      <FeedContainer />
+    </CommonContainer>
+  );
+};
+
+export default FeedPage;
