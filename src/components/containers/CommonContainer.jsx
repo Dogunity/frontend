b56ff216dc0f4e54a5cbd82c45510b63bc2fd868
foxmon <jjockrod@naver.com>
2022-12-07 21:49:08 +0900

    Fix: CommonContainer

--- src/components/containers/CommonContainer.jsx
+++ src/components/containers/CommonContainer.jsx
@@ -1,11 +1,13 @@
 import { Box } from "@mui/material";
 import MyHeader from "../commonLayout/MyHeader";
+import MyFooter from "../commonLayout/MyFooter";
 
 const CommonContainer = ({ children }) => {
   return (
     <Box>
       <MyHeader />
       {children}
+      <MyFooter />
     </Box>
   );
 };
