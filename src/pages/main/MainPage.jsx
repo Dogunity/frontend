974ae911a3960c892b427d483890f5854314a7f3
foxmon <jjockrod@naver.com>
2022-12-07 21:48:26 +0900

    Fix: MainPage

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,5 +1,9 @@
-import { Typography, Box, Container } from "@mui/material";
+import { Typography, Box, Container, Stack, Button, Grid } from "@mui/material";
 import CommonContainer from "../../components/containers/CommonContainer";
+import CommunityCardContainer from "../../components/containers/CommunityCardContainer";
+
+// temp map
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const MainPage = () => {
   return (
@@ -25,8 +29,26 @@ const MainPage = () => {
             the creator, etc. Make it short and sweet, but not too short so
             folks don&apos;t simply skip over it entirely.
           </Typography>
+          <Stack
+            sx={{ pt: 4 }}
+            direction="row"
+            spacing={2}
+            justifyContent="center"
+          >
+            <Button variant="contained">Explore our community</Button>
+            <Button variant="outlined">Join our community</Button>
+          </Stack>
         </Container>
       </Box>
+      <Container sx={{ py: 8 }} maxWidth="md">
+        <Grid container spacing={4}>
+          {cards.map((card) => (
+            <Grid item key={card} xs={12} sm={6} md={4}>
+              <CommunityCardContainer />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </CommonContainer>
   );
 };
