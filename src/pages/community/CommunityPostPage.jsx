15f25a5da78e66b4a16a179efe252b240de23a8c
foxmon <jjockrod@naver.com>
2022-12-12 21:41:18 +0900

    Feat: CommunityPostPage

---
+++ src/pages/community/CommunityPostPage.jsx
@@ -0,0 +1,12 @@
+import CommonContainer from "../../components/containers/CommonContainer";
+import CommunityForm from "../../components/community/CommunityForm";
+
+const CommunityPostPage = () => {
+  return (
+    <CommonContainer>
+      <CommunityForm />
+    </CommonContainer>
+  );
+};
+
+export default CommunityPostPage;
