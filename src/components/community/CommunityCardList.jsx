d995ffc88c15012b0f814284737523a46dbf8e45
foxmon <jjockrod@naver.com>
2022-12-12 22:13:50 +0900

    Fix: CommunityCardList

--- src/components/community/CommunityCardList.jsx
+++ src/components/community/CommunityCardList.jsx
@@ -3,10 +3,10 @@ import CommunityCardContainer from "../containers/CommunityCardContainer";
 
 const CommunityCardList = ({ communities }) => {
   return (
-    <Grid container spacing={4}>
+    <Grid container spacing={4} sx={{ mt: 10 }}>
       {communities.map((community) => (
         <Grid item key={community.id} sm={6} md={4}>
-          <CommunityCardContainer />
+          <CommunityCardContainer community={community} />
         </Grid>
       ))}
     </Grid>
