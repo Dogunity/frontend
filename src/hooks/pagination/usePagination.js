36a2823374bb44e8dd092dbdcfd946665b68ebbd
foxmon <jjockrod@naver.com>
2022-12-12 22:12:52 +0900

    Fix: usePagination

--- src/hooks/pagination/usePagination.js
+++ src/hooks/pagination/usePagination.js
@@ -2,8 +2,8 @@ import { useMemo, useCallback } from "react";
 
 const usePagination = (curPage, setCurPage, totalPage, divider) => {
   const isFirstPage = useMemo(() => curPage === 1, [curPage]);
-  const isLastPage = useMemo(() => curPage === totalPage, [totalPage]);
-  const initialPageNum = (Math.ceil(curPage / divider) + 1) * divider + 1;
+  const isLastPage = useMemo(() => curPage === totalPage, [curPage, totalPage]);
+  const initialPageNum = (Math.ceil(curPage / divider) - 1) * divider + 1;
   const pageListIndex = Array.from({ length: divider })
     .fill(0)
     .map((_, index) =>
