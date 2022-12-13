263a076608ce2c6f82d8abf808b4f47d15aadf2f
foxmon <jjockrod@naver.com>
2022-12-13 23:05:14 +0900

    Fix: CommunityCardFooter

--- src/components/community/CommunityCardFooter.jsx
+++ src/components/community/CommunityCardFooter.jsx
@@ -1,10 +1,27 @@
-import { CardActions, Button } from "@mui/material";
+import { CardActions, Typography, Button, IconButton } from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
-const CommunityCardFooter = () => {
+const CommunityCardFooter = ({
+  community,
+  onCardViewButtonClickEvent,
+  onCommunityLikeButtonClickEvent,
+}) => {
   return (
     <CardActions>
-      <Button size="small">View</Button>
-      <Button size="small">Edit</Button>
+      <IconButton
+        onClick={(e) => onCommunityLikeButtonClickEvent(e, community.id)}
+      >
+        <FavoriteIcon />
+        <Typography variant="h6" color="text.secondary">
+          {community.likeCnt}
+        </Typography>
+      </IconButton>
+      <Button
+        size="small"
+        onClick={(e) => onCardViewButtonClickEvent(e, community)}
+      >
+        View
+      </Button>
     </CardActions>
   );
 };
