416faf461d8c050732cb978ba3a2ac598c960c1a
foxmon <jjockrod@naver.com>
2022-12-13 21:59:44 +0900

    Fix: CommunityPostPage

--- src/pages/community/CommunityPostPage.jsx
+++ src/pages/community/CommunityPostPage.jsx
@@ -1,10 +1,26 @@
+import useCommunity from "../../hooks/community/useCommunity";
 import CommonContainer from "../../components/containers/CommonContainer";
 import CommunityForm from "../../components/community/CommunityForm";
 
 const CommunityPostPage = () => {
+  const {
+    register,
+    errors,
+    imagePreview,
+    handleSubmit,
+    handleCommunityCreateSubmit,
+    handleImageUploadClick,
+  } = useCommunity();
+
   return (
     <CommonContainer>
-      <CommunityForm />
+      <CommunityForm
+        register={register}
+        errors={errors}
+        imagePreview={imagePreview}
+        onImageUploadClickEvent={handleImageUploadClick}
+        onCommunityCreateSubmitEvent={handleSubmit(handleCommunityCreateSubmit)}
+      />
     </CommonContainer>
   );
 };
