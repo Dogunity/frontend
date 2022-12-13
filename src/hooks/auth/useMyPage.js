daf9bf0feb8b6310fe26b4005f532c4083f80b42
foxmon <jjockrod@naver.com>
2022-12-14 00:11:53 +0900

    Fix: useMyPage

--- src/hooks/auth/useMyPage.js
+++ src/hooks/auth/useMyPage.js
@@ -1,7 +1,13 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
+import useSessionStorage from "../common/useSessionStorage";
+import AuthCommunityGrid from "../../components/auth/community/AuthCommunityGrid";
+import { authMyPageCommuityRequest } from "../../apis/authService";
 
 const useMyPage = () => {
   const [tabIndex, setTabIndex] = useState(0);
+  const [communities, setCommunities] = useState([]);
+
+  const { item } = useSessionStorage();
 
   const tabItems = [
     {
@@ -12,6 +18,52 @@ const useMyPage = () => {
     },
   ];
 
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "name",
+      headerName: "Name",
+      width: 200,
+      editable: true,
+    },
+    {
+      field: "introduction",
+      headerName: "Introduction",
+      width: 300,
+      editable: true,
+    },
+    {
+      field: "createdAt",
+      headerName: "CreatedAt",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "likeCnt",
+      headerName: "Like",
+      width: 120,
+      editable: true,
+    },
+  ];
+
+  useEffect(() => {
+    (async () => {
+      const res = await authMyPageCommuityRequest(1, item);
+      const { myCommunities } = res;
+      if (!myCommunities.length) return;
+      const data = myCommunities.map((com) => {
+        return {
+          id: com.id,
+          name: com.name,
+          introduction: com.introduction,
+          createdAt: com.createdAt.split("T")[0],
+          likeCnt: com.likeCnt,
+        };
+      });
+      setCommunities(data);
+    })();
+  }, [tabIndex]);
+
   const handleTabIndexChange = useCallback(
     (_, newValue) => {
       setTabIndex(newValue);
@@ -19,10 +71,26 @@ const useMyPage = () => {
     [setTabIndex]
   );
 
+  const getComponentByTabIndex = () => {
+    switch (tabIndex) {
+      case 0:
+        return <AuthCommunityGrid columns={columns} rows={communities} />;
+
+      case 1:
+        return <div>Tab 2</div>;
+
+      default:
+        break;
+    }
+  };
+
   return {
     tabIndex,
     tabItems,
+    columns,
+    communities,
     handleTabIndexChange,
+    getComponentByTabIndex,
   };
 };
 
