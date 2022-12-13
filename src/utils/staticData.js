3cb2ab1681732a12e92da3bb894004bb71fef82b
foxmon <jjockrod@naver.com>
2022-12-14 01:21:09 +0900

    Feat: staticData

---
+++ src/utils/staticData.js
@@ -0,0 +1,70 @@
+export const afterLoginMenuItems = [
+  {
+    title: "Community",
+    path: "/community",
+  },
+  {
+    title: "My page",
+    path: "/mypage",
+  },
+  {
+    title: "Sign out",
+    path: "/",
+  },
+];
+
+export const beforeLoginMenuItems = [
+  {
+    title: "Community",
+    path: "/community",
+  },
+  {
+    title: "My page",
+    path: "/mypage",
+  },
+  {
+    title: "Sign in",
+    path: "/login",
+  },
+  {
+    title: "Sign up",
+    path: "/register",
+  },
+];
+
+export const tabItems = [
+  {
+    label: "My Communities",
+  },
+  {
+    label: "Accounts",
+  },
+];
+
+export const columns = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "name",
+    headerName: "Name",
+    width: 200,
+    editable: true,
+  },
+  {
+    field: "introduction",
+    headerName: "Introduction",
+    width: 300,
+    editable: true,
+  },
+  {
+    field: "createdAt",
+    headerName: "CreatedAt",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "likeCnt",
+    headerName: "Like",
+    width: 120,
+    editable: true,
+  },
+];
