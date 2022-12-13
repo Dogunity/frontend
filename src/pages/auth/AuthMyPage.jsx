a20d6257bf1ebf009af5b2b5a7321f8d6e1981d9
foxmon <jjockrod@naver.com>
2022-12-14 01:21:44 +0900

    Fix: AuthMyPage

--- src/pages/auth/AuthMyPage.jsx
+++ src/pages/auth/AuthMyPage.jsx
@@ -1,11 +1,25 @@
 import useMyPage from "../../hooks/auth/useMyPage";
 import CommonContainer from "../../components/containers/CommonContainer";
 import AuthMyPageTabs from "../../components/auth/common/AuthMyPageTabs";
-import AuthCommunityGrid from "../../components/auth/community/AuthCommunityGrid";
+import CustomModal from "../../components/modal/MyCustomModal";
+import CommunityEditForm from "../../components/community/CommunityEditForm";
 
 const AuthMyPage = () => {
-  const { tabIndex, tabItems, handleTabIndexChange, getComponentByTabIndex } =
-    useMyPage();
+  const {
+    tabIndex,
+    tabItems,
+    selectedItem,
+    isModalOpen,
+    errors,
+    imagePreview,
+    register,
+    handleSubmit,
+    handleCommunityUpdateSubmit,
+    handleTabIndexChange,
+    handleIsModalOpenStateChange,
+    handleImageUploadClick,
+    getComponentByTabIndex,
+  } = useMyPage();
 
   return (
     <CommonContainer>
@@ -15,6 +29,22 @@ const AuthMyPage = () => {
         onTabIndexChangeEvent={handleTabIndexChange}
       />
       {getComponentByTabIndex()}
+      <CustomModal
+        isModalOpen={isModalOpen}
+        title="Your Community Detail"
+        onIsModalOpenStateChangeEvent={handleIsModalOpenStateChange}
+      >
+        <CommunityEditForm
+          selectedItem={selectedItem}
+          register={register}
+          errors={errors}
+          imagePreview={imagePreview}
+          onCommunityUpdateSubmitEvent={handleSubmit(
+            handleCommunityUpdateSubmit
+          )}
+          onImageUploadClickEvent={handleImageUploadClick}
+        />
+      </CustomModal>
     </CommonContainer>
   );
 };
