5532306a52d54c30e00aa18fe97a6c62f4627d37
foxmon <jjockrod@naver.com>
2022-12-13 23:34:00 +0900

    Fix: FeedPage

--- src/pages/feed/FeedPage.jsx
+++ src/pages/feed/FeedPage.jsx
@@ -1,10 +1,19 @@
+import useFeed from "../../hooks/feed/useFeed";
 import CommonContainer from "../../components/containers/CommonContainer";
 import FeedContainer from "../../components/containers/FeedContainer";
 
 const FeedPage = () => {
+  const { name, introduction, communityImage, handleFeedImageButtonClick } =
+    useFeed();
+
   return (
     <CommonContainer>
-      <FeedContainer />
+      <FeedContainer
+        name={name}
+        introduction={introduction}
+        communityImage={communityImage}
+        onFeedImageButtonClickEvent={handleFeedImageButtonClick}
+      />
     </CommonContainer>
   );
 };
