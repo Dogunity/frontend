542125eed5689d6d2ada74a6b69b043cbe46c4b4
foxmon <jjockrod@naver.com>
2022-12-13 22:00:18 +0900

    Fix: CommunityCardList

--- src/components/community/CommunityCardList.jsx
+++ src/components/community/CommunityCardList.jsx
@@ -1,15 +1,40 @@
-import { Grid } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Box, Grid, Button } from "@mui/material";
+import useSessionStorage from "../../hooks/common/useSessionStorage";
 import CommunityCardContainer from "../containers/CommunityCardContainer";
 
 const CommunityCardList = ({ communities }) => {
+  const { item } = useSessionStorage();
+
+  const navigate = useNavigate();
+
   return (
-    <Grid container spacing={4} sx={{ mt: 10 }}>
-      {communities.map((community) => (
-        <Grid item key={community.id} sm={6} md={4}>
-          <CommunityCardContainer community={community} />
-        </Grid>
-      ))}
-    </Grid>
+    <Box>
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          mt: 12,
+        }}
+      >
+        {item && (
+          <Button
+            variant="outlined"
+            onClick={() => navigate("/community/post")}
+          >
+            Create community
+          </Button>
+        )}
+      </Box>
+      <Grid container spacing={4} sx={{ mt: 2 }}>
+        {communities.map((community) => (
+          <Grid item key={community.id} sm={6} md={4}>
+            <CommunityCardContainer community={community} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
 
