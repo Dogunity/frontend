74f76d7d1f86f4fa4639e859b54f8929761ee079
foxmon <jjockrod@naver.com>
2022-12-15 00:11:36 +0900

    Fix: FeedPage

--- src/pages/feed/FeedPage.jsx
+++ src/pages/feed/FeedPage.jsx
@@ -1,18 +1,49 @@
 import useFeed from "../../hooks/feed/useFeed";
 import CommonContainer from "../../components/containers/CommonContainer";
 import FeedContainer from "../../components/containers/FeedContainer";
+import PaginatorButton from "../../components/paginator/PaginatorButton";
+import MyCustommodal from "../../components/modal/MyCustomModal";
 
 const FeedPage = () => {
-  const { name, introduction, communityImage, handleFeedImageButtonClick } =
-    useFeed();
+  const {
+    id,
+    name,
+    introduction,
+    communityImage,
+    curPage,
+    totalPage,
+    feeds,
+    selectedFeedItem,
+    isModalOpen,
+    setCurPage,
+    handleFeedImageButtonClick,
+    handleIsModalOpenStateChange,
+  } = useFeed();
+
+  console.log(selectedFeedItem);
 
   return (
     <CommonContainer>
       <FeedContainer
+        id={id}
         name={name}
         introduction={introduction}
         communityImage={communityImage}
+        feeds={feeds}
         onFeedImageButtonClickEvent={handleFeedImageButtonClick}
+      />
+      <MyCustommodal
+        isModalOpen={isModalOpen}
+        title="Create your feed"
+        onIsModalOpenStateChangeEvent={handleIsModalOpenStateChange}
+      >
+        <div>아직 없음</div>
+      </MyCustommodal>
+      <PaginatorButton
+        curPage={curPage}
+        setCurPage={setCurPage}
+        totalPage={totalPage}
+        divider={9}
       />
     </CommonContainer>
   );
