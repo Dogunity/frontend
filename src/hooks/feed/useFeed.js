f461089fe4cc4f1c22efbcc33972755cfd848ad4
foxmon <jjockrod@naver.com>
2022-12-15 00:10:20 +0900

    Fix: useFeed

--- src/hooks/feed/useFeed.js
+++ src/hooks/feed/useFeed.js
@@ -1,25 +1,54 @@
-import { useState, useMemo, useCallback } from "react";
+import { useState, useEffect, useMemo, useCallback } from "react";
 import { useLocation } from "react-router-dom";
+import useSessionStorage from "../common/useSessionStorage";
+import useModal from "../common/useModal";
+import { feedItemListRequest } from "../../apis/feedService";
 
 const useFeed = () => {
   const [selectedFeedItem, setSelectedFeedItem] = useState(null);
+  const [curPage, setCurPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
+  const [feeds, setFeeds] = useState([]);
 
   const { state } = useLocation();
-  const { name, introduction, communityImage } = useMemo(() => state, [state]);
+
+  const { id, name, introduction, communityImage } = useMemo(
+    () => state,
+    [state]
+  );
+
+  const { item } = useSessionStorage();
+  const { isModalOpen, handleIsModalOpenStateChange } = useModal();
+
+  useEffect(() => {
+    (async () => {
+      const res = await feedItemListRequest(id, curPage, item);
+      const { totalFeedPage, foundPosts } = res.result;
+      setFeeds(foundPosts);
+      setTotalPage(totalFeedPage);
+    })();
+  }, [id, curPage, item]);
 
   const handleFeedImageButtonClick = useCallback(
-    (_, selectedItem) => {
+    (selectedItem) => () => {
       setSelectedFeedItem(selectedItem);
-      console.log(selectedFeedItem);
     },
     [setSelectedFeedItem]
   );
 
   return {
+    id,
     name,
     introduction,
     communityImage,
+    curPage,
+    totalPage,
+    feeds,
+    selectedFeedItem,
+    isModalOpen,
+    setCurPage,
     handleFeedImageButtonClick,
+    handleIsModalOpenStateChange,
   };
 };
 
