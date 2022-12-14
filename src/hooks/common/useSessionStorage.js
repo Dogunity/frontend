b5fd4d6d5d86f795c49e7c52b5d9bf5e72828fa9
foxmon <jjockrod@naver.com>
2022-12-15 00:47:34 +0900

    Fix: useSessionStorage

--- src/hooks/common/useSessionStorage.js
+++ src/hooks/common/useSessionStorage.js
@@ -1,11 +1,17 @@
 import { useState, useCallback } from "react";
 
 const ACCESS_TOKEN_KEY = "ACCESS_TOKEN_KEY";
+const USER_ID = "USER_ID";
 
 const useSessionStorage = () => {
   const [item, setItem] = useState(() => {
     if (sessionStorage.getItem(ACCESS_TOKEN_KEY) !== null)
       return sessionStorage.getItem(ACCESS_TOKEN_KEY);
+    return null;
+  });
+  const [userId, setUserId] = useState(() => {
+    if (sessionStorage.getItem(USER_ID) !== null)
+      return sessionStorage.getItem(USER_ID);
     return null;
   });
 
@@ -19,15 +25,28 @@ const useSessionStorage = () => {
     [setItem]
   );
 
+  const setUserIdSessionStorageItem = useCallback(
+    (value) => {
+      if (!value) return;
+      if (typeof value === "undefined" || typeof value !== "number") return;
+      setUserId(value);
+      sessionStorage.setItem(USER_ID, value);
+    },
+    [setUserId]
+  );
+
   const clearSessionStorageItem = useCallback(() => {
     if (!item) return;
     setItem(null);
+    setUserId(null);
     sessionStorage.clear();
-  }, [item, setItem]);
+  }, [item, setItem, setUserId]);
 
   return {
     item,
+    userId,
     setSessionStorageItem,
+    setUserIdSessionStorageItem,
     clearSessionStorageItem,
   };
 };
