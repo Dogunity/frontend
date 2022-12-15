b8b0eee89be748f9b7f6b91f761d2bc3d28494df
foxmon <jjockrod@naver.com>
2022-12-15 21:42:32 +0900

    Fix: FeedDetail

--- src/components/feed/FeedDetail.jsx
+++ src/components/feed/FeedDetail.jsx
@@ -1,45 +1,67 @@
 import { Box, Typography, Button, IconButton } from "@mui/material";
 import useSessionStorage from "../../hooks/common/useSessionStorage";
+import useCustomForm from "../../hooks/common/useCustomForm";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import MyCustomImageSlider from "../common/MyCustomImageSlider";
+import CommentList from "../comment/CommentList";
+import CommentForm from "../comment/CommentForm";
+import { commentPostRequest } from "../../apis/commentService";
 
 const FeedDetail = ({
   feed,
   onFeedDeleteButtonClickEvent,
   onFeedLikeButtonClickEvent,
 }) => {
-  const { userId } = useSessionStorage();
+  const { item, userId } = useSessionStorage();
+  const { formValue, handleFormValueChange } = useCustomForm();
+
+  const handleFormValueSubmit = async (e) => {
+    e.preventDefault();
+    const { comment } = formValue;
+    const res = await commentPostRequest(feed.id, comment, item);
+    if (res.status >= 200 && res.status < 300) window.location.reload();
+  };
 
   return (
-    <Box
-      sx={{
-        width: 1000,
-        height: 600,
-        margin: "0 auto",
-        textAlign: "center",
-      }}
-    >
-      <MyCustomImageSlider images={feed.images} />
-      <Box sx={{ width: "100%" }}>
-        <Typography variant="h5" color="text.primary">
-          {feed.description}
-        </Typography>
+    <Box sx={{ display: "flex", justifyContent: "center", p: 1 }}>
+      <Box
+        sx={{
+          width: 800,
+          height: 600,
+          margin: "0 auto",
+          textAlign: "center",
+        }}
+      >
+        <MyCustomImageSlider images={feed.images} />
+        <Box sx={{ width: "100%" }}>
+          <Typography variant="h5" color="text.primary">
+            {feed.description}
+          </Typography>
+        </Box>
+        <IconButton onClick={(e) => onFeedLikeButtonClickEvent(e, feed.id)}>
+          <FavoriteIcon />
+          <Typography variant="h6" color="text.secondary">
+            {feed.likeCnt}
+          </Typography>
+        </IconButton>
+        <Box>
+          {parseInt(userId) === feed.userId && (
+            <Button
+              variant="contained"
+              onClick={onFeedDeleteButtonClickEvent(feed)}
+            >
+              Delete
+            </Button>
+          )}
+        </Box>
       </Box>
-      <IconButton onClick={(e) => onFeedLikeButtonClickEvent(e, feed.id)}>
-        <FavoriteIcon />
-        <Typography variant="h6" color="text.secondary">
-          {feed.likeCnt}
-        </Typography>
-      </IconButton>
-      <Box>
-        {parseInt(userId) === feed.userId && (
-          <Button
-            variant="contained"
-            onClick={onFeedDeleteButtonClickEvent(feed)}
-          >
-            Delete
-          </Button>
-        )}
+      <Box sx={{ width: 300 }}>
+        <CommentList feedId={feed.id} />
+        <CommentForm
+          formValue={formValue}
+          onFormValueChangeEvent={handleFormValueChange}
+          onFormValueSubmitEvent={handleFormValueSubmit}
+        />
       </Box>
     </Box>
   );
