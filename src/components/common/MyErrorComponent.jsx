2b5324abbc6b46567662ed32262c48bb13b7d3a4
foxmon <jjockrod@naver.com>
2022-12-15 21:57:28 +0900

    Fix: MyErrorComponent

--- src/components/common/MyErrorComponent.jsx
+++ src/components/common/MyErrorComponent.jsx
@@ -1,8 +1,24 @@
+import { Box, Typography, Link } from "@mui/material";
+
 function MyErrorComponent({ error }) {
   return (
-    <div>
-      <p>This is Error {error}</p>
-    </div>
+    <Box
+      sx={{
+        width: "100%",
+        height: "90vh",
+        textAlign: "center",
+      }}
+    >
+      <Box sx={{ mt: 30 }}>
+        <Typography variant="h3">OOPS</Typography>
+        <Box sx={{ mt: 2 }}>
+          <Typography variant="h5">Something wrong {error}</Typography>
+          <Box sx={{ mt: 5 }}>
+            <Link href="/">GO BACK</Link>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
